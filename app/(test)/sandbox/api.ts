ef6131ed773823a63efab31015bfdec79972a0c3
NyeowsZ <nyeowsz@gmail.com>
2026-06-15 21:54:24 +0800

    feat: add googleai api logic and a prompt test ui
    - working loading states on ui
    - function handleAPI returns gemma and gemini text dynamically
    - thinking is set to minimal to minimize latency
    - return response condition parts[1].text and parts[0].text's due to gemma and gemini json structure difference

---
+++ app/(test)/sandbox/api.ts
@@ -0,0 +1,30 @@
+"use server";
+import axios from "axios";
+import "dotenv/config";
+
+export const handleAPI = async (prompt: string) => {
+  const apiKey = process.env.GEMINI_API_KEY;
+  const model = process.env.GEMINI_AI_MODEL;
+
+  const response = await axios.post(
+    `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent`,
+    {
+      contents: [{ parts: [{ text: prompt }] }],
+      generationConfig: { thinkingConfig: { thinkingLevel: "minimal" } },
+    },
+    {
+      headers: {
+        "x-goog-api-key": apiKey,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const res = response.data.candidates[0].content;
+
+  if (res.parts[1]?.text) {
+    return res.parts[1].text;
+  } else {
+    return res.parts[0].text;
+  }
+};
--- app/(test)/sandbox/page.tsx
+++ app/(test)/sandbox/page.tsx
@@ -1,46 +1,32 @@
 "use client";
 import { useState } from "react";
+import { handleAPI } from "./api";
 
 const Page = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const [apiLink, setApiLink] = useState<string>("");
-  const [apiKey, setApiKey] = useState<string>("");
   const [prompt, setPrompt] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
 
   if (process.env.NODE_ENV != "development") return null;
 
-  const handle = () => {
+  const handle = async () => {
     setIsLoading(true);
-    setTimeout(() => {
+    setResponse("");
+
+    try {
+      const res = await handleAPI(prompt);
+      setResponse(res);
+    } catch (error) {
+      console.log(error);
+    } finally {
       setIsLoading(false);
-    }, 2000);
+    }
   };
 
   return (
     <>
       <div className="flex flex-col items-start gap-5">
         <h1 className="text-2xl font-semibold">LLM API Test</h1>
-
-        <div className="space-y-1">
-          <h2 className="text-xl font-semibold">API Auth</h2>
-          <div className="flex flex-col items-start">
-            <input
-              type="text"
-              placeholder="API Link"
-              className="px-5 py-2 rounded-xl border border-neutral-500"
-              value={apiLink}
-              onChange={(e) => setApiLink(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="API Key"
-              className="px-5 py-2 rounded-xl border border-neutral-500"
-              value={apiKey}
-              onChange={(e) => setApiKey(e.target.value)}
-            />
-          </div>
-        </div>
 
         <input
           type="text"
@@ -54,7 +40,7 @@ const Page = () => {
           <button
             className={`px-5 py-2 rounded-xl  text-white font-semibold  ${!isLoading ? "hover:bg-blue-600 active:bg-blue-700 bg-blue-500 cursor-pointer" : "bg-neutral-500"}`}
             disabled={isLoading}
-            onClick={handle}
+            onClick={() => handle()}
           >
             Send Prompt
           </button>
@@ -62,6 +48,7 @@ const Page = () => {
             <span className="block border-3 border-blue-500 border-t-white animate-spin size-5 rounded-full" />
           )}
         </div>
+        <p>{response ? response : "No response"}</p>
       </div>
     </>
   );
