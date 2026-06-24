4fefa5ddd57451cbc494784f39c18516b361e94c
NyeowsZ <nyeowsz@gmail.com>
2026-06-25 02:20:54 +0800

    refactor: extract inline sandbox base64 logic to components/Base64.tsx

---
+++ app/(test)/sandbox/components/Base64.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { documentToBase64 } from "../documentToBase64";
+
+const Base64 = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [file, setFile] = useState<File>();
+  const [base64, setBase64] = useState("");
+
+  const handleDocumentToBase64 = async () => {
+    try {
+      setIsLoading(true);
+      if (file) {
+        try {
+          const response = await documentToBase64(file);
+          setBase64(response);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-start gap-2.5 mt-10">
+      <h1 className="text-2xl font-semibold">Base64 Test</h1>
+      <input
+        type="file"
+        onChange={(e) => {
+          if (e.target.files && e.target.files.length) {
+            setFile(e.target.files[0]);
+          }
+        }}
+        className="px-2 py-2 rounded-xl file:bg-blue-300 border file:px-2 file:py-2 file:rounded-xl border-neutral-500 cursor-pointer"
+      />
+      <p>
+        Size:{" "}
+        {file
+          ? `${(file.size / 1024 / 1024).toFixed(2)}MB + ${((file.size / 1024 / 1024) * 0.33).toFixed(2)}MB (base64 33% conversion tax) = ${(file.size / 1024 / 1024 + (file.size / 1024 / 1024) * 0.33).toFixed(2)}MB`
+          : "--"}
+      </p>
+      <button
+        className={`px-5 py-2 rounded-xl  text-white font-semibold  ${!isLoading ? "hover:bg-blue-600 active:bg-blue-700 bg-blue-500 cursor-pointer" : "bg-neutral-500"}`}
+        disabled={isLoading}
+        onClick={() => handleDocumentToBase64()}
+      >
+        Parse
+      </button>
+      <p>{base64 ? base64.slice(0, 40) + "..." : "No parsed base64"}</p>
+    </div>
+  );
+};
+
+export default Base64;
