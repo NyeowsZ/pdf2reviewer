5956f71de2ae242d0ad5f6391e8cdd98b3834d4d
NyeowsZ <nyeowsz@gmail.com>
2026-06-25 02:15:45 +0800

    refactor: replace inline llm api logic with component
    - use components/Llm.tsx import and component

--- app/(test)/sandbox/page.tsx
+++ app/(test)/sandbox/page.tsx
@@ -1,31 +1,14 @@
 "use client";
 import { useState } from "react";
-import { handleAPI } from "./api";
 import { documentToBase64 } from "./documentToBase64";
+import Llm from "./components/Llm";
 
 const Page = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [prompt, setPrompt] = useState<string>("");
-  const [response, setResponse] = useState<string>("");
-
   const [file, setFile] = useState<File>();
   const [base64, setBase64] = useState("");
 
   if (process.env.NODE_ENV != "development") return null;
-
-  const handle = async () => {
-    setIsLoading(true);
-    setResponse("");
-
-    try {
-      const res = await handleAPI(prompt);
-      setResponse(res);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   const handleDocumentToBase64 = async () => {
     try {
@@ -45,31 +28,7 @@ const Page = () => {
 
   return (
     <>
-      <div className="flex flex-col items-start gap-5">
-        <h1 className="text-2xl font-semibold">LLM API Test</h1>
-
-        <input
-          type="text"
-          placeholder="Prompt"
-          className="px-5 py-2 rounded-xl border border-neutral-500"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-
-        <div className="flex items-center gap-2.5">
-          <button
-            className={`px-5 py-2 rounded-xl  text-white font-semibold  ${!isLoading ? "hover:bg-blue-600 active:bg-blue-700 bg-blue-500 cursor-pointer" : "bg-neutral-500"}`}
-            disabled={isLoading}
-            onClick={() => handle()}
-          >
-            Send Prompt
-          </button>
-          {isLoading && (
-            <span className="block border-3 border-blue-500 border-t-white animate-spin size-5 rounded-full" />
-          )}
-        </div>
-        <p>{response ? response : "No response"}</p>
-      </div>
+      <Llm />
 
       <div className="flex flex-col items-start gap-2.5 mt-10">
         <h1 className="text-2xl font-semibold">Base64 Test</h1>
